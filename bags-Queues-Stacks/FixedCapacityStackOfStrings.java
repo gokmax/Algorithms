444f6c0e959332ca96c78fa409acd4786b3099a3
gokmax <zenglingzheng@gmail.com>
2013-02-20 12:35:01 +0800

    add the next folder bags-Queues-Stacks

---
+++ bags-Queues-Stacks/FixedCapacityStackOfStrings.java
@@ -0,0 +1,23 @@
+public class FixedCapacityStackOfStrings {
+
+    private String[] s;
+    private int N = 0;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+        s = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(String item) {
+        s[N++] = item;
+    }
+
+    public String pop() {
+        String item = s[--N];
+        s[N] = null; 
+        return item;
+    }
+}
