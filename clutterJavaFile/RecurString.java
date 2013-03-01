5e426158b92ba94eb29cfa0fac7e6232e7a2ffb3
gokmax <zenglingzheng@gmail.com>
2013-03-01 15:11:42 +0800

    add the RecurString.java

---
+++ clutterJavaFile/RecurString.java
@@ -0,0 +1,14 @@
+public class RecurString {
+
+    public static String mystery(String s) {
+        int N = s.length();
+        if (N <= 1) return s;
+        String a = s.substring(0, N / 2);
+        String b = s.substring(N / 2, N);
+        return mystery(b) + mystery(a);
+    }
+
+    public static void main (String [] args) {
+        StdOut.println(mystery("abcdefg"));
+    }
+}
