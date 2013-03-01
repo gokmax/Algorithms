c617796404531f5f4ff0ee0e6cac807cff44ea95
gokmax <zenglingzheng@gmail.com>
2013-03-01 19:18:39 +0800

    add the change index of the array to myBsInsertionSort.java to see the stability of it

--- sorting/myBsInsertionSort.java
+++ sorting/myBsInsertionSort.java
@@ -2,8 +2,12 @@ import java.util.Arrays;
 
 public class myBsInsertionSort{
 
-    public static void sort(int[] a) {
+    public static int[] sort(int[] a) {
         int N = a.length;
+
+        int[] index = new int[N];
+        for (int i = 0; i < N; i ++) index[i] = i;
+
         for (int i = 1; i < N; i ++) {
             int key = a[i];
 
@@ -24,6 +28,7 @@ public class myBsInsertionSort{
             if (a[low] == key) continue; // it means that the a[i]'s original place is exactly the right place.
             for (int k = i; k > mid ; --k) {
                 a[k] = a[k - 1];
+                index[k] = index[k - 1];
             }
 
             a[low] = key;
@@ -34,15 +39,21 @@ public class myBsInsertionSort{
             //}
             //a[j] = key;
         }
+
+        return index;
     }
 
     public static void main (String [] args) {
         int[] a = {3, 3, 2, 1, 5, 5, 6, 7, 4};
+        int[] b = new int[9];
+        for (int i = 0; i < 9; i ++) b[i] = a[i];
         //int[] index = {0, 1, 2, 3, 4, 5, 6, 7, 8};
         //int[] a = {1, 2, 3, 5, 6};
-        myBsInsertionSort.sort(a);
+        int[] index = new int[9];
+        index = myBsInsertionSort.sort(a);
         int N = a.length;
-        for (int i = 0; i < N; i ++) StdOut.println(a[i]);
+        for (int i = 0; i < N; i ++) StdOut.println(b[i] + " " + a[i] + " " + index[i]);
+
     }
 }
             // the index `low` is where the key should be put in.
