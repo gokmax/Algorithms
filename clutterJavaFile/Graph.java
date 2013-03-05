bb15eb21cebb94b382c818a6ce732b0135e79323
gokmax <zenglingzheng@gmail.com>
2013-03-05 19:27:15 +0800

    be more cautious about your study.

--- clutterJavaFile/Graph.java
+++ clutterJavaFile/Graph.java
@@ -1,3 +1,8 @@
+/*
+ * need more condition.
+ * be careful.
+ */
+
 import java.util.Arrays;
 
 public class Graph {
@@ -5,32 +10,33 @@ public class Graph {
     public static boolean sort(int[] a) {
 
         int len = a.length;
-        int sum1 = 0;
+        int originalSum = 0;
         for (int i = 0; i < len - 1; i ++) {
             if (a[i] < 0) return false;
             if (a[i] < a[i + 1]) return false;
-            sum1 += a[i];
+            originalSum += a[i];
         }
         if (a[len - 1] < 0) return false;
-        sum1 += a[len - 1];
-        if (sum1 % 2 != 0) return false;
+        originalSum += a[len - 1];
+        if (originalSum % 2 != 0) return false;
         // initialized graph judgement ends here.
 
         int d1 = a[0];
-        int sum2 = 0;
+        int newSum = 0;
         for (int i = 1; i <= d1; i ++) {
             a[i] --;
             if (a[i] < 0) return false;
-            sum2 += a[i];
+            newSum += a[i];
         }
         for (int i = d1 + 1; i < len; i ++)
-            sum2 += a[i];
-        if (sum2 % 2 != 0) return false;
+            newSum += a[i];
+        if (newSum % 2 != 0) return false;
 
         return true;
     }
 
     public static void main (String [] args) {
+
         int[] a = {5, 5, 4, 3, 2, 1};
         StdOut.println(Graph.sort(a));
     }
